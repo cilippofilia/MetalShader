fa2655e1d095aa7fb716d682148f0f73f26d2399
Filippo Cilia <filippocarlocilia@gmail.com>
2026-02-07 20:46:24 +0000

    Initial Commit

---
+++ MetalShaderTest/MetalShaderTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalShaderTestApp.swift
+//  MetalShaderTest
+//
+//  Created by Filippo Cilia on 07/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct MetalShaderTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
